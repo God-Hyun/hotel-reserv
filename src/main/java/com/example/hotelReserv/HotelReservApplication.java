444b28c362b8d389c73c69c1870ca5f37747b80e
God-Hyun <ghgu1381@gmail.com>
2024-08-29 10:01:50 +0900

    feat: first commit - SpringBoot프로젝트 빌드

---
+++ src/main/java/com/example/hotelReserv/HotelReservApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotelReserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelReservApplication.class, args);
+	}
+
+}
